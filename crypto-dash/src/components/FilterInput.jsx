34d818ecda5af5c92e61cd8c410f0d411b984814
JSRobertOuimet <jsrobertouimet@gmail.com>
2025-08-09 22:22:19 -0400

    End of section 7.

--- crypto-dash/src/components/FilterInput.jsx
+++ crypto-dash/src/components/FilterInput.jsx
@@ -1,12 +1,15 @@
 const FilterInput = ({ filter, onChange }) => {
     return (
         <div className="filter">
-            <label htmlFor="search" style={{ position: "absolute", left: "-9999px" }}>
+            <label
+                htmlFor="search"
+                style={{ position: "absolute", left: "-9999px" }}>
                 Search
             </label>
             <input
                 type="search"
                 id="search"
+                placeholder="Search by name or symbol"
                 value={filter}
                 onChange={e => onChange(e.target.value)}
             />
