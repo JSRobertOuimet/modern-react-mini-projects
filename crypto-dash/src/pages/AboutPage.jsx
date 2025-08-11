a4d43c89a12209b70ef962784431bd793f930fd8
JSRobertOuimet <jsrobertouimet@gmail.com>
2025-08-10 20:48:22 -0400

    End of lesson 72.

---
+++ crypto-dash/src/pages/AboutPage.jsx
@@ -0,0 +1,22 @@
+const AboutPage = () => {
+    return (
+        <div className="about">
+            <h1>About Crypto Dash</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Commodi vero, veniam deleniti nostrum doloribus beatae
+                aliquid, non ab, quam obcaecati quia quidem nobis.
+                Tempore, quos placeat perspiciatis excepturi asperiores
+                veritatis aperiam in modi aspernatur iste, rerum nobis
+                consectetur! Ipsum quaerat amet consectetur nesciunt,
+                ipsa voluptate.
+            </p>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Optio, fuga?
+            </p>
+        </div>
+    );
+};
+
+export default AboutPage;
