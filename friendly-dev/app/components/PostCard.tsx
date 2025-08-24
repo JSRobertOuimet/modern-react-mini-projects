60ffec8b8edbbef983702c3daffa8f1b90062855
JSRobertOuimet <jsrobertouimet@gmail.com>
2025-08-24 16:51:47 -0400

    End of lesson 125.

---
+++ friendly-dev/app/components/PostCard.tsx
@@ -0,0 +1,24 @@
+import type { PostMeta } from "~/types";
+import { Link } from "react-router";
+
+const PostCard = ({post}: {post: PostMeta}) => {
+    return (
+        <article
+            className="bg-gray-800 mb-4 p-6 rounded-lg shadow">
+            <h2 className="text-2xl-font-semibold text-blue-400">
+                {post.title}
+            </h2>
+            <p className="text-sm text-gray-400 mb-2">
+                {new Date(post.date).toLocaleDateString()}
+            </p>
+            <p className="text-gray-300 mb-4">{post.excerpt}</p>
+            <Link
+                to={`/blog/${post.slug}`}
+                className="text-blue-300 text-sm hover:underline">
+                Read More
+            </Link>
+        </article>
+    );
+}
+ 
+export default PostCard;
--- friendly-dev/app/routes/contact/index.tsx
+++ friendly-dev/app/routes/contact/index.tsx
@@ -1,8 +1,129 @@
-const ContactPage = () => {
+import type { Route } from "./+types";
+import { Form } from "react-router";
+
+export async function action({ request }: Route.ActionArgs) {
+    const formData = await request.formData();
+
+    const name = formData.get("name") as string;
+    const email = formData.get("email") as string;
+    const subject = formData.get("subject") as string;
+    const message = formData.get("message") as string;
+
+    const errors: Record<string, string> = {};
+
+    if (!name) errors.name = "Name is required.";
+    if (!email) {
+        errors.email = "Email is required.";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        errors.email = "Email address is invalid.";
+    }
+    if (!subject) errors.subject = "Subject is required.";
+    if (!message) errors.message = "Message is required.";
+
+    if (Object.keys(errors).length > 0) {
+        return { errors };
+    }
+
+    const data = {
+        name,
+        email,
+        subject,
+        message,
+    };
+
+    return { message: "Form submitted successfully!", data };
+}
+
+const ContactPage = ({ actionData }: Route.ComponentProps) => {
+    const errors = actionData?.errors || {};
+
     return (
-        <>
-            <h1 className="text-3xl font-bold mb-2">Contact</h1>
-        </>
+        <div className="max-w-3xl mx-auto mt-12 px-6 py-8 bg-gray-900">
+            <h1 className="text-3xl font-bold mb-8">Contact</h1>
+
+            {actionData?.message ? (
+                <p className="p-2 my-6 bg-green-100 text-green-900 rounded rounded-lg border border-green-500 shadow-md">
+                    {actionData.message}
+                </p>
+            ) : null}
+
+            <Form method="post" className="space-y-6">
+                <div>
+                    <label
+                        htmlFor="name"
+                        className="block text-sm font-medium text-gray-300">
+                        Name
+                    </label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+                    />
+                    {errors.name && (
+                        <p className="text-red-400 text-sm mt-1">
+                            {errors.name}
+                        </p>
+                    )}
+                </div>
+                <div>
+                    <label
+                        htmlFor="email"
+                        className="block text-sm font-medium text-gray-300">
+                        Email Address
+                    </label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+                    />
+                    {errors.email && (
+                        <p className="text-red-400 text-sm mt-1">
+                            {errors.email}
+                        </p>
+                    )}
+                </div>
+                <div>
+                    <label
+                        htmlFor="subject"
+                        className="block text-sm font-medium text-gray-300">
+                        Subject
+                    </label>
+                    <input
+                        type="text"
+                        id="subject"
+                        name="subject"
+                        className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+                    />
+                    {errors.subject && (
+                        <p className="text-red-400 text-sm mt-1">
+                            {errors.subject}
+                        </p>
+                    )}
+                </div>
+                <div>
+                    <label
+                        htmlFor="message"
+                        className="block text-sm font-medium text-gray-300">
+                        Message
+                    </label>
+                    <textarea
+                        id="message"
+                        name="message"
+                        className="w-full mt-1 px-4 py-2 border border-gray-700 rounded-lg bg-gray-800 text-gray-100"
+                    />
+                    {errors.message && (
+                        <p className="text-red-400 text-sm mt-1">
+                            {errors.message}
+                        </p>
+                    )}
+                </div>
+                <button className="w-full bg-blue-600 text-white py-2 rounded-lg bg-blue-600 hover:bg-blue-700 cursor-pointer">
+                    Send Message
+                </button>
+            </Form>
+        </div>
     );
 };
 
